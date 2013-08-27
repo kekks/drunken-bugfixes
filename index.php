cf7d824059f74f46a6079780f49212033f011189
Francesco Chessa Orrù <francescochessaorru@gmail.com>
2013-08-27 10:20:35 +0200

    took out ds store

--- index.php
+++ index.php
@@ -69,6 +69,7 @@
             <option>ARMANI JEANS</option>
             <option>ARMANI JUNIOR</option>
             <option>ARMANI COLLEZIONI</option>
+            <option>ARMANI PRIVÈ</option>
             <option>ARMANI DOLCI</option>
             <option>ARMANI FIORI</option>
             <option>ARMANI CASA</option>
