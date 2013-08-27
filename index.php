79e0043cd11ccad6a2f09470759c130ca0398bc8
Steven McCrumb <steve@pofd.com>
2013-08-27 11:29:43 +0200

    changed out latin e

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
             <option>ARMANI JEANS</option>
             <option>ARMANI JUNIOR</option>
             <option>ARMANI COLLEZIONI</option>
-            <option>ARMANI PRIV&Egrave</option>
+            <option>ARMANI PRIVE'</option>
             <option>ARMANI DOLCI</option>
             <option>ARMANI FIORI</option>
             <option>ARMANI CASA</option>
